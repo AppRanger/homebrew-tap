c33056bda36be9b6d5ea1db9523e19327be8cf22
Chris Hobden <cd@3v.io>
2026-08-30 20:05:55 +0100

    Add WindowRanger 1.0.0 Cask

---
+++ Casks/windowranger.rb
@@ -0,0 +1,29 @@
+cask "windowranger" do
+  version "1.0.0"
+  sha256 "387f50a42a542729f27efb6c473e771568bb0701e049e71d3c6210ff0215f604"
+
+  url "https://github.com/AppRanger/windowranger/releases/download/v#{version}/WindowRanger-#{version}.dmg",
+      verified: "github.com/AppRanger/windowranger/"
+  name "WindowRanger"
+  desc "Native workspace and window manager"
+  homepage "https://windowranger.com/"
+
+  auto_updates true
+  depends_on macos: :sonoma
+
+  app "WindowRanger.app"
+
+  uninstall quit: "dev.appranger.WindowRanger"
+
+  zap trash: [
+    "~/Library/Caches/com.windowranger.WindowRanger",
+    "~/Library/Caches/dev.appranger.WindowRanger",
+    "~/Library/HTTPStorages/dev.appranger.WindowRanger",
+    "~/Library/HTTPStorages/dev.appranger.WindowRanger.binarycookies",
+    "~/Library/Logs/dev.appranger.WindowRanger",
+    "~/Library/Preferences/com.windowranger.WindowRanger.plist",
+    "~/Library/Preferences/dev.appranger.WindowRanger.plist",
+    "~/Library/Saved Application State/com.windowranger.WindowRanger.savedState",
+    "~/Library/Saved Application State/dev.appranger.WindowRanger.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# AppRanger Homebrew Tap
+
+Homebrew packages for [AppRanger](https://appranger.com/) macOS applications.
+
+## WindowRanger
+
+Install the latest signed and notarized Stable release:
+
+```sh
+brew install --cask appranger/tap/windowranger
+```
+
+WindowRanger includes its own signed Stable updater. Homebrew and WindowRanger use the same
+versioned release artifact and application identity.
+
+- [WindowRanger website](https://windowranger.com/)
+- [WindowRanger source and releases](https://github.com/AppRanger/windowranger)
